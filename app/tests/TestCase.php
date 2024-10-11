0b542462ed787992f8b167a273884a304b49e726
Ruslan <r.gusseinov@mail.ru>
2024-10-12 00:27:19 +0500

    add vars in tests

--- app/tests/TestCase.php
+++ app/tests/TestCase.php
@@ -6,5 +6,16 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+/*     public function setUp(): void
+    {
+        parent::setUp();
+        
+        // Output the DB credentials being used for testing
+        echo "DB_CONNECTION: " . env('DB_CONNECTION') . "\n";
+        echo "DB_HOST: " . env('DB_HOST') . "\n";
+        echo "DB_PORT: " . env('DB_PORT') . "\n";
+        echo "DB_DATABASE: " . env('DB_DATABASE') . "\n";
+        echo "DB_USERNAME: " . env('DB_USERNAME') . "\n";
+        echo "DB_PASSWORD: " . env('DB_PASSWORD') . "\n";
+    } */
 }
