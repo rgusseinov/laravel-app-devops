88541ec3c3259fc1277603f6db6f8257f0c815e1
Ruslan <r.gusseinov@mail.ru>
2024-10-15 18:26:55 +0500

    correct user page label

--- app/resources/views/users/index.blade.php
+++ app/resources/views/users/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 <h1>Users</h1>
-<p>List of users</p>
+<p>List of system users!</p>
 <ul>
     @foreach ($users as $user)
         <li><a href="/users/{{ $user->id }}">{{ $user->name }}, {{ $user->email }}</a></li>
