71e069ed7576fa8b71306614e673e92cb89078df
Ruslan <r.gusseinov@mail.ru>
2024-10-15 17:43:04 +0500

    add page label

--- app/resources/views/users/index.blade.php
+++ app/resources/views/users/index.blade.php
@@ -3,6 +3,7 @@
 @section('content')
 
 <h1>Users</h1>
+<p>List of users</p>
 <ul>
     @foreach ($users as $user)
         <li><a href="/users/{{ $user->id }}">{{ $user->name }}, {{ $user->email }}</a></li>
